6fa41f84662ad91bf3e958eb2a3ed66862efe267
williamazevedo <william@vipcommerce.com.br>
2021-05-15 04:07:11 -0300

    script para facilitar inicializar na producao

---
+++ run_production.sh
@@ -0,0 +1,1 @@
+git pull origin main && sudo docker-compose -f docker-compose.yml -f docker-compose.production.override.yml build && sudo docker-compose -f docker-compose.yml -f docker-compose.production.override.yml up -d
